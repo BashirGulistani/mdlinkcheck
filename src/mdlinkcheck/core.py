31a2435d57c1b5ec9fa0b30433f26e166f09e607
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-02-09 23:06:44 -0500

    Update core.py

--- src/mdlinkcheck/core.py
+++ src/mdlinkcheck/core.py
@@ -14,6 +14,31 @@ REF_USE_RE = re.compile(r"\[([^\]]+)\]\[([^\]]+)\]")
 AUTOLINK_RE = re.compile(r"<(https?://[^ >]+)>")
 
 
+def _is_markdown(path):
+    return os.path.splitext(path)[1].lower() in MD_EXTENSIONS
+
+def _walk_markdown_files(root, include, exclude_substrings):
+    candidates = []
+
+    def allowed(p):
+        norm = p.replace("\\", "/")
+        for s in exclude_substrings:
+            if s and s in norm:
+                return False
+        return True
+
+    if include:
+        for item in include:
+            if os.path.isfile(item) and _is_markdown(item) and allowed(item):
+                candidates.append(item)
+            elif os.path.isdir(item) and allowed(item):
+                for base, _, files in os.walk(item):
+                    if not allowed(base):
+                        continue
+                    for f in files:
+                        fp = os.path.join(base, f)
+                        if _is_markdown(fp) and allowed(fp):
+                            candidates.append(fp)
 
 
 
